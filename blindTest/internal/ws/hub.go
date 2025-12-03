aa1038bbb871d3c2286c8eb485ade66d1c9f0f10
Ludivine Goirand <ludivine@ynov.com>
2025-12-03 11:39:59 +0100

    a ete fait le hub en go

--- blindTest/internal/ws/hub.go
+++ blindTest/internal/ws/hub.go
@@ -1,1 +1,129 @@
 package ws
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+)
+
+type Message struct {
+	Type    string                 `json:"type"`
+	UserID  int64                  `json:"userID,omitempty"`
+	Payload map[string]interface{} `json:"payload,omitempty"`
+}
+
+type Hub struct {
+	clients    map[*Client]bool
+	Register   chan *Client
+	Unregister chan *Client
+	Broadcast  chan []byte
+	Input      chan Message
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		clients:    make(map[*Client]bool),
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Broadcast:  make(chan []byte, 64),
+		Input:      make(chan Message, 64),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case c := <-h.Register:
+			h.clients[c] = true
+		case c := <-h.Unregister:
+			if _, ok := h.clients[c]; ok {
+				delete(h.clients, c)
+				close(c.send)
+			}
+		case msg := <-h.Broadcast:
+			for c := range h.clients {
+				select {
+				case c.send <- msg:
+				default:
+					delete(h.clients, c)
+					close(c.send)
+				}
+			}
+		}
+	}
+}
+
+func (h *Hub) BroadcastJSON(m Message) {
+	b, _ := json.Marshal(m)
+	h.Broadcast <- b
+}
+
+type Client struct {
+	hub  *Hub
+	conn *websocket.Conn
+	send chan []byte
+}
+
+func NewClient(hub *Hub, conn *websocket.Conn) *Client {
+	return &Client{
+		hub:  h,
+		conn: conn,
+		send: make(chan []byte, 256),
+	}
+}
+
+func (c *Client) ReadPump() {
+	defer func() {
+		c.hub.Unregister <- c
+		c.conn.Close()
+	}()
+	c.conn.SetReadLimit(1 << 20)
+	c.conn.SetReadDeadline(time.Now().Add(60 * time.Second))
+	c.conn.SetPongHandler(func(string) error {
+		c.conn.SetReadDeadline(time.Now().Add(60 * time.Second))
+		return nil
+	})
+	for {
+		var m Message
+		if err := c.conn.ReadJSON(&m); err != nil {
+			log.Println("read:", err)
+			return
+		}
+		c.hub.Input <- m
+	}
+}
+
+func (c *Client) WritePump() {
+	ticker := time.NewTicker(45 * time.Second)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+
+			if _, err := w.Write(message); err != nil {
+				return
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
